f0d4c71be4325c206b37adabe6334a4733c324e3
Madhusudan23082000 <73375985+Madhusudan23082000@users.noreply.github.com>
2022-01-29 13:33:07 +0530

    initiate

---
+++ src/components/about/About.jsx
@@ -0,0 +1,43 @@
+import "./about.css";
+import Award from "../../img/award.png";
+
+const About = () => {
+  return (
+    <div className="a">
+      <div className="a-left">
+        <div className="a-card bg"></div>
+        <div className="a-card">
+          <img
+            src="https://images.pexels.com/photos/3585047/pexels-photo-3585047.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            alt=""
+            className="a-img"
+          />
+        </div>
+      </div>
+      <div className="a-right">
+        <h1 className="a-title">About Me</h1>
+        <p className="a-sub">
+          It is a long established fact that a reader will be distracted by the
+          readable content.
+        </p>
+        <p className="a-desc">
+          I have done my BE from Vidyavardhini college of engg and tech,
+          through this time I have learned various technologies
+          for building websites as well as different commercial apps.
+          
+        </p>
+        <div className="a-award">
+          <img src={Award} alt="" className="a-award-img" />
+          <div className="a-award-texts">
+            <h4 className="a-award-title">Internships in various companies</h4>
+            <p className="a-award-desc">
+              BMUCO and Peka consultancy.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+  {
+    id: 1,
+    img: "images/code_1.jpeg",
+    link: "code_1.jpeg",
+  },
+  {
+    id: 2,
+    img: "images/code_2.jpeg",
+    link: "code_2.jpeg",
+  },
+  {
+    id: 3,
+    img: "images/code_3.jpeg",
+    link: "code_3.jpeg",
+  },
+  {
+    id: 4,
+    img: "images/code_4.jpeg",
+    link: "code_4.jpeg",
+  },
+  {
+    id: 5,
+    img: "images/code_5.jpeg",
+    link: "code_5.jpeg",
+  },
+  {
+    id: 6,
+    img: "images/code_6.jpeg",
+    link: "code_6.jpeg",
+  },
+];
